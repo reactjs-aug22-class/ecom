163a329378e9f9c9f01861dafa2e0c49ec4c40f3
Amr Fouad <3mrfouad@gmail.com>
2022-11-15 22:04:00 -0700

    implement cart persistance using local storage api

--- src/components/Context/UserCartProvider.jsx
+++ src/components/Context/UserCartProvider.jsx
@@ -1,9 +1,21 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import UserCartContext from './UserCartContext'
 
 export default function UserCartProvider({ children }) {
-  const [userCart, setUserCart] = useState([])
+  const [userCart, setCart] = useState([])
+
+  useEffect(() => {
+    const savedUserCart = JSON.parse(localStorage.getItem('cart'))
+    if (savedUserCart && Array.isArray(savedUserCart)) {
+      setCart(savedUserCart)
+    }
+  }, [])
+
+  const setUserCart = value => {
+    setCart(value)
+    localStorage.setItem('cart', JSON.stringify(value))
+  }
 
   const value = {
     userCart,
