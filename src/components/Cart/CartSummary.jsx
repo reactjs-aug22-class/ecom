3051f39270e36d70d0aad192fe04f26b80212124
Amr Fouad <3mrfouad@gmail.com>
2022-11-15 22:02:20 -0700

    implement cart summary subtotal and checkout logic

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -2,21 +2,33 @@ import './cartSummary.stylesheet.css'
 
 import React, { useContext } from 'react'
 
+import { Link } from 'react-router-dom'
 import UserCartContext from '../Context/UserCartContext'
 
 export default function CartSummary() {
-  const { userCart } = useContext(UserCartContext)
+  const { userCart, setUserCart } = useContext(UserCartContext)
+  const isDisabledCheckoutBtn = userCart.length === 0
   const subTotal = userCart.reduce((total, item) => {
-    console.log(item, total)
-    return (total += parseFloat(item.price))
+    return (total += parseFloat(item.price * item.qty))
   }, 0)
   return (
     <div className="cart-summary">
       <div className="cart-summary-price">
         <p className="all-caps">Subtotal</p>
-        <p>$ {subTotal}</p>
+        <p>$ {subTotal.toFixed(2)}</p>
       </div>
-      <button className="btn cart-summary-btn all-caps">Checkout</button>
+      <Link to={'/checkout'}>
+        <button
+          className={`btn cart-summary-btn all-caps 
+            ${isDisabledCheckoutBtn ? 'grey-out' : ''}`}
+          disabled={isDisabledCheckoutBtn}
+          onClick={() => {
+            setUserCart([])
+          }}
+        >
+          Checkout
+        </button>
+      </Link>
     </div>
   )
 }
