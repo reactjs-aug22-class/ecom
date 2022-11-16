afa436c9868a291633d2900b022434cfe8e285e6
Amr Fouad <3mrfouad@gmail.com>
2022-11-15 22:03:12 -0700

    implement remove button logic

--- src/components/MarketPlace/MarketPlaceCard.jsx
+++ src/components/MarketPlace/MarketPlaceCard.jsx
@@ -5,20 +5,41 @@ import styles from './marketPlaceCard.stylesheet.css'
 
 export default function MarketPlaceCard({ product }) {
   const { userCart, setUserCart } = useContext(UserCartContext)
+
   const handleAddToCart = () => {
     const index = userCart.findIndex(item => item.id === product.id)
     if (index === -1) {
-      setUserCart(prevUserCart => [...prevUserCart, { ...product, qty: 1 }])
+      const userCartCopy = [...userCart, { ...product, qty: 1 }]
+      setUserCart(userCartCopy)
     }
   }
+
+  const handleRemoveFromCart = () => {
+    const index = userCart.findIndex(item => item.id === product.id)
+    if (index !== -1) {
+      const userCartCopy = [...userCart]
+      userCartCopy.splice(index, 1)
+      setUserCart(userCartCopy)
+    }
+  }
+
+  const isInCart = userCart.findIndex(item => item.id === product.id) !== -1
+
   return (
     <div className="marketplace-card">
       <img className="marketplace-card-img" src={product.image} />
-      <p>{product.title}</p>
+      <p className="product-title">{product.title}</p>
       <p className="marketplace-card-price">$ {product.price}</p>
-      <button onClick={handleAddToCart} className="btn marketplace-btn">
-        Add to cart
-      </button>
+
+      {isInCart ? (
+        <button onClick={handleRemoveFromCart} className="btn marketplace-btn">
+          Remove From cart
+        </button>
+      ) : (
+        <button onClick={handleAddToCart} className="btn marketplace-btn">
+          Add to cart
+        </button>
+      )}
     </div>
   )
 }
